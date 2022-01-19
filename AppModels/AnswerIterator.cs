95d3bac66e685c460b4088b5279ae2beb0b1ff5b
Maciej Sobolewski <macieksob25@gmail.com>
2022-01-19 19:47:46 +0100

    iterator

---
+++ AppModels/AnswerIterator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZTPWordsProject.AppModels
+{
+    class AnswerIterator : IIterator
+    {
+        private int currentPosition = 0;
+        private List<Answers> answers = new List<Answers>;
+        public Answers GetNext()
+        {
+            if (!HasNext())
+            {
+                return null;
+            }
+            Answers answer = answers.Find(currentPosition);
+            currentPosition++;
+
+            return answer;
+
+        }
+
+        public bool HasNext()
+        {
+            currentPosition < answers.Count;
+
+        }
+    }
+}
